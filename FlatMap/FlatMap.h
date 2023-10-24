71f0c84590dbd4c9c055d29b76f320d8c38a30a7
irina <i.savelova@g.nsu.ru>
2023-10-24 22:02:36 +0700

    without quick sort

--- FlatMap/FlatMap.h
+++ FlatMap/FlatMap.h
@@ -19,12 +19,55 @@ public:
     FlatMap(const FlatMap& b) : capacity(b.capacity), sizeArray(b.sizeArray)//конструктор копирования
     {
         data = new std::pair<Key, Value>[capacity];
-        
+
         for (size_t i = 0; i < sizeArray; ++i)
         {
             data[i] = b.data[i];
         }
     }
+    FlatMap(const FlatMap& b);
+    FlatMap(FlatMap&& b);
+    FlatMap& operator=(const FlatMap& b)
+    {// Если не совпадает capacity, то нужно вызвать resize()
+        if (this == &b)
+            return *this;
+        if (sizeArray == b.sizeArray)
+        {
+            for (size_t i = 0; i < sizeArray; ++i)
+                data[i] = b.data[i];
+            capacity = b.capacity;
+            DefaultSize = b.DefaultSize;
+            return *this;
+        }
+        else if (sizeArray > b.sizeArray)
+        {
+            for (size_t i = 0; i < b.sizeArray; ++i)
+                data[i] = b.data[i];
+            sizeArray = b.sizeArray;
+            capacity = b.capacity;
+            DefaultSize = b.DefaultSize;
+            return *this;
+        }
+        else // (sizeArray < b.sizeArray)
+        {
+            if (capacity < b.capacity)
+                resize();
+            sizeArray = b.sizeArray;
+            for (size_t i = 0; i < sizeArray; ++i)
+                data[i] = b.data[i];
+            capacity = b.capacity;
+            DefaultSize = b.DefaultSize;
+            return *this;
+        }
+    }
+    FlatMap&& operator=(FlatMap&& b)
+    {
+        // почистить исходный массив, скопировать значения всех свойств, после чего
+        // data = b.data;
+    }
+    
+    
+    void swap(FlatMap& b);
     void clear()
     {
         if (sizeArray == 0) return;
@@ -35,16 +78,17 @@ public:
     // Удаляет элемент по заданному ключу.
     bool erase(const Key& key)
     {
-        size_t index = BinarySearch(key);
+        bool ok;
+        size_t index = BinarySearch(key, ok);
+        if (!ok)
+            return false; // Ключ не найден, удаление не выполнено
         if (data[index].first == key)
         {
-            // Перемещаем последний элемент на место удаляемого
-            data[index] = data[sizeArray - 1];
+            for (size_t i = index; i < sizeArray - 1; ++i)
+                data[i] = data[i + 1];
             sizeArray--;
-            quickSort(data); //?????????????????
             return true; // Успешно удалено
         }
-        return false; // Ключ не найден, удаление не выполнено
     }
     // Вставка в контейнер. Возвращаемое значение - успешность вставки.
     bool insert(const Key& key, const Value& value)
@@ -54,63 +98,57 @@ public:
             // Если массив заполнен, увеличиваем его размер
             resize();
         }
-
-        if (sizeArray < capacity)
-        {
-            data[sizeArray] = std::make_pair(key, value);
-            sizeArray++;
-            quickSort(data); //?????????????????
-            return true; // Успешно вставлено
-        }
         else
         {
-            return false; // Не удалось вставить, так как массив уже полон
+            bool ok;
+            size_t index = BinarySearch(key, ok);
+            if (ok)
+            {
+                data[index] = std::make_pair(key, value);
+                return true;
+            }
+            sizeArray++;
+            for (size_t i = index + 1; i < sizeArray - 1; ++i)
+                data[i] = data[i - 1];
+            data[index] = std::make_pair(key, value);
+            return true; // Успешно вставлено
         }
     }
 
     // Проверка наличия значения по заданному ключу
     bool contains(const Key& key) const
     {
-        size_t index = BinarySearch(key);
-        if (data[index].first == key)
+        bool ok;
+        size_t index = BinarySearch(key, ok);
+        if (ok && data[index].first == key)
         {
             return true;
         }
         else
-        return false;
+            return false;
     }
     // Возвращает значение по ключу. Бросает исключение при неудаче.
     Value at(const Key& key) const
     {
-        try {
-            size_t index = BinarySearch(key);
-            if (data[index].first == key)
-            {
-                return data[index].second;
-            }
+        bool ok;
+        size_t index = BinarySearch(key, ok);
+        if (ok && data[index].first == key)
+        {
+            return data[index].second;
         }
-        catch (const std::out_of_range& e) {
-            // Обработка исключения std::out_of_range
-            std::cerr << "Key not found" << e.what() << std::endl;
-            // Дополнительные действия по обработке ошибки, если необходимо
-        }
-        //throw std::out_of_range("Key not found");
+        throw std::out_of_range("Key not found");
     }
-    //const Value& at(const Key& key) const
-    //{
-        //const size_t index = BinarySearch(key);
-        //if (data[index].first == key)
-            //return data.second;
-        //else throw std::out_of_range("Key isn't available");
-    //}
+    
+    
     // Возвращает значение по ключу. Небезопасный метод.
       // В случае отсутствия ключа в контейнере, следует вставить в контейнер
       // значение, созданное конструктором по умолчанию и вернуть ссылку на него.
     Value& operator[](const Key& key)
     {
-        size_t index = BinarySearch(key);
+        bool ok;
+        size_t index = BinarySearch(key, ok);
 
-        if (data[index].first == key)
+        if (ok && data[index].first == key)
         {
             // Ключ найден, возвращаем соответствующее значение
             return data[index].second;
@@ -122,23 +160,24 @@ public:
             return data[sizeArray - 1].second; // Возвращаем ссылку на новое значение
         }
     }
+    
     size_t size() const
     {
         return sizeArray;
     }
-    bool empty() const
+    bool empty() const //че
     {
         return sizeArray == 0;
     }
 
 private:
     std::pair<Key, Value>* data;
-    size_t capacity = 0ull;;
-    size_t sizeArray = 0ull;;
+    size_t capacity = 0ull;
+    size_t sizeArray = 0ull;
     static constexpr size_t DefaultSize = 1ull;
 
 
-// Увеличение емкости массива при заполнении
+    // Увеличение емкости массива при заполнении
     void resize()
     {
         capacity *= 2;
@@ -151,23 +190,24 @@ private:
         data = newData;
     }
 
-    size_t BinarySearch(const Key k) const
+    size_t BinarySearch(const Key k, bool& ok) const
     {
-       if (sizeArray == 0) return 0;
-       if (k == data[0].first) return 0;
-       if (k == data[sizeArray - 1].first) return sizeArray - 1;
+        if (sizeArray == 0) { ok = true; return 0; }
+        if (k == data[0].first) { ok = true; return 0; }
+        if (k == data[sizeArray - 1].first) { ok = true;  return sizeArray - 1; }
 
-       size_t r = sizeArray - 1;
-       size_t l = 0;
-       size_t mid = 0;
-       while (l < r)
-       {
-           mid = l + ((r - l) / 2);
-           if (k == data[mid].first) return mid;
-           else if (k > data[mid].first) l = mid + 1;
-           else r = mid;
-       }
-       return l;
+        size_t r = sizeArray - 1;
+        size_t l = 0;
+        size_t mid = 0;
+        while (l <= r)
+        {
+            mid = (l + r) / 2;
+            if (k == data[mid].first) { ok = true;  return mid; }
+            else if (k > data[mid].first) l = mid + 1;
+            else r = mid - 1;
+        }
+        ok = false;
+        return l;
     }
     int partition(std::pair<Key, Value>* data, int start, int end)
     {
@@ -188,12 +228,12 @@ private:
             }
         }
         // поменять местами `pIndex` с пивотом
-        swap (data[pIndex], data[end]);
-     
+        swap(data[pIndex], data[end]);
+
         // вернуть `pIndex` (индекс опорного элемента)
         return pIndex;
     }
-     
+
     // Процедура быстрой сортировки
     void quickSort(std::pair<Key, Value>* data, int start, int end)
     {
@@ -203,42 +243,38 @@ private:
         }
         // переставить элементы по оси
         int pivot = partition(data, start, end);
-     
+
         // повторяем подмассив, содержащий элементы, меньшие опорной точки
         quickSort(data, start, pivot - 1);
-     
+
         // повторяем подмассив, содержащий элементы, превышающие точку опоры
         quickSort(data, pivot + 1, end);
     }
-FlatMap(FlatMap&& b);
-void swap(FlatMap& b);
+    FlatMap(FlatMap&& b);
+    void swap(FlatMap& b);
 
-FlatMap& operator=(const FlatMap& b);
-FlatMap&& operator=(FlatMap&& b);
-friend bool operator==(const FlatMap& a, const FlatMap& b);
-friend bool operator!=(const FlatMap& a, const FlatMap& b);
+
+    friend bool operator==(const FlatMap& a, const FlatMap& b);
+    friend bool operator!=(const FlatMap& a, const FlatMap& b);
 };
-bool operator==(const FlatMap& a, const FlatMap& b){//оператор сравнения (дружественные операторы)
-    if(a.sizeArray==b.sizeArray){
-        for(int i =0; i < a.sizeArray; i++){
+bool operator==(const FlatMap& a, const FlatMap& b) {//оператор сравнения (дружественные операторы)
+    if (a.sizeArray == b.sizeArray) {
+        for (int i = 0; i < a.sizeArray; i++) {
             if!(a.data[i].first == b.data[i].first) return false;
             if!(a.data[i].second == b.data[i].second) return false
         }
-        
+        return true;
     }
     else
         return false;
 }
-bool operator!=(const FlatMap& a, const FlatMap& b){//оператор сравнения (дружественные операторы)
-    if(a.sizeArray!=b.sizeArray) return true
-    if(a.sizeArray==b.sizeArray){
-        for(int i =0; i < a.sizeArray; i++){
-            if!(a.data[i].first == b.data[i].first) return true;
-            if!(a.data[i].second == b.data[i].second) return true;
-        }
-        
+bool operator!=(const FlatMap& a, const FlatMap& b) {//оператор сравнения (дружественные операторы)
+    if (a.sizeArray != b.sizeArray) return true;
+    for (int i = 0; i < a.sizeArray; i++) {
+        if!(a.data[i].first == b.data[i].first) return true;
+        if!(a.data[i].second == b.data[i].second) return true;
     }
-    else
-        return false;
+    return false;
 }
 #endif /* FlatMap_h */
+
